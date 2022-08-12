6032f1744e80f5fcb19eae79a2596f7b4cf85ff0
5H4ND <miqueiasantos20@gmail.com>
2022-08-12 15:57:37 -0300

    msm8937-common: Uprev perf HIDL to 2.2

--- msm8937.mk
+++ msm8937.mk
@@ -315,8 +315,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES+= \
-    vendor.qti.hardware.perf@2.0 \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.2 \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \

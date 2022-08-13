cbc628738f3fd5b2b759679e152549e719ef8d0f
5H4ND <miqueiasantos20@gmail.com>
2022-08-13 16:42:37 -0300

    msm8937-common: Add dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,6 @@
+git clone -b twelve git@github.com:5H4ND/android_vendor_motorola.git vendor/motorola;
+git clone -b twelve https://github.com/5H4ND/device_motorola_msm8937-common device/motorola/msm8937-common;
+git clone -b twelve https://github.com/5H4ND/device_motorola_hannah-common device/motorola/hannah-common;
+git clone -b lineage-18.1-rebase-2 https://github.com/GKart/msm-4.9 kernel/motorola/msm8953;
+git clone -b lineage-19.1 https://github.com/LineageOS/android_hardware_motorola hardware/motorola;
+

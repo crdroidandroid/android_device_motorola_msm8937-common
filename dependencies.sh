e260bc0b2ec9939247084530a6b1f539121c8e9c
5H4ND <miqueiasantos20@gmail.com>
2022-08-20 08:05:43 -0300

    msm8937-common: Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,10 @@
+# Trees
 git clone -b twelve git@github.com:5H4ND/android_vendor_motorola.git vendor/motorola;
-git clone -b twelve https://github.com/5H4ND/device_motorola_msm8937-common device/motorola/msm8937-common;
-git clone -b twelve https://github.com/5H4ND/device_motorola_hannah-common device/motorola/hannah-common;
-git clone -b lineage-18.1-rebase-2 https://github.com/GKart/msm-4.9 kernel/motorola/msm8953;
-git clone -b lineage-19.1 https://github.com/LineageOS/android_hardware_motorola hardware/motorola;
+git clone -b twelve git@github.com:5H4ND/device_motorola_hannah-common device/motorola/hannah-common;
+git clone -b twelve git@github.com:5H4ND/kernel_motorola_msm8953 kernel/motorola/msm8953;
 
+# LineageOS
+git clone -b lineage-19.1 https://github.com/LineageOS/android_hardware_motorola hardware/motorola;
+git clone -b lineage-19.1-caf-msm8996 https://github.com/LineageOS/android_hardware_qcom_display hardware/qcom-caf/msm8996/display;
+git clone -b lineage-19.1-caf-msm8996 https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom-caf/msm8996/audio
+git clone -b lineage-19.1-caf-msm8996 https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom-caf/msm8996/media;

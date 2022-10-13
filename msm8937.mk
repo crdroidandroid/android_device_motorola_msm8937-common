66de0f37046d8e9d71821d3fa057037b8574bf09
5H4ND <miqueiasantos20@gmail.com>
2022-10-13 19:30:52 -0300

    Revert "msm8937-common: Build mock hidl power stats"
    This reverts commit fb55f7c16135d745c378bcc4fdc9f456626785ab.

--- msm8937.mk
+++ msm8937.mk
@@ -317,7 +317,6 @@ endif
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power@1.2.vendor
 
 # Perf
